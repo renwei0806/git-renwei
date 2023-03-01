ae5d41abc1f2a5b54d7ab7e27b514a7a2d44ef80
your username <2360881524@qq.com>
2023-03-01 11:01:30 +0800

    提交备注

---
+++ js/page.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+    message: 'Hello Vue!'
+    },
+    methods:{
+        todo:function(){
+            // alert("www.baidu.com")
+            window.location.href="https://www.baidu.com"
+        }
+    }
+})
+
+
+    
